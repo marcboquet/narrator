480ab9c2e1fea2dd03e199aa0245f7e22ca3c271
Marc Boquet <marc.boquet@gmail.com>
2019-05-11 22:44:29 +0200

    Send to thread

--- src/narrator.js
+++ src/narrator.js
@@ -4,14 +4,22 @@ const http = require("https")
 exports.handler = function(event, context, callback) {
   const parsed = qa.parse(event.body)
   console.log("parsed:", parsed)
-  const text = parsed.text
+  let text = parsed.text
+  let threadId, url
+  const threadmatch = text.match(/https?:\/\/[^\s]+thread_ts=([\d\.]+)[^\s]+/)
+  if (threadmatch) {
+    url = threadmatch[0]
+    threadId = threadmatch[1]
+  }
+  text = text.replace(url, "")
   const token = process.env.BOT_TOKEN
   console.log("token:", token)
 
-  const data = JSON.stringify({
+  let data = JSON.stringify({
     channel: parsed.channel_id,
     text: `_${text}_`
   })
+  if (threadId) data.thread_ts = threadId
 
   const opts = {
     hostname: "slack.com",
