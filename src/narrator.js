610064918082a57712c35c69e9cc1df6ec0313b3
Marc Boquet <marc.boquet@gmail.com>
2019-05-11 22:01:43 +0200

    prettier

--- src/narrator.js
+++ src/narrator.js
@@ -1,17 +1,17 @@
-const qa = require("qs");
-const http = require("https");
+const qa = require("qs")
+const http = require("https")
 
 exports.handler = function(event, context, callback) {
-  const parsed = qa.parse(event.body);
-  console.log("parsed is ", parsed);
-  const text = parsed.text;
-  const token = process.env.BOT_TOKEN;
-  console.log(`token = '${token}'`);
+  const parsed = qa.parse(event.body)
+  console.log("parsed:", parsed)
+  const text = parsed.text
+  const token = process.env.BOT_TOKEN
+  console.log("token:", token)
 
   const data = JSON.stringify({
     channel: parsed.channel_id,
     text: `_${text}_`
-  });
+  })
 
   const opts = {
     hostname: "slack.com",
@@ -23,22 +23,22 @@ exports.handler = function(event, context, callback) {
       "Content-Length": data.length,
       Authorization: `Bearer ${token}`
     }
-  };
+  }
 
   const req = http.request(opts, res => {
-    console.log(`statusCode: ${res.statusCode}`);
+    console.log(`statusCode: ${res.statusCode}`)
 
     res.on("data", d => {
-      process.stdout.write(d);
-    });
+      process.stdout.write(d)
+    })
 
-    callback(null, { body: "", statusCode: 200 });
-  });
+    callback(null, { body: "", statusCode: 200 })
+  })
 
   req.on("error", e => {
-    console.log("error is ", e);
-  });
+    console.log("error is ", e)
+  })
 
-  req.write(data);
-  req.end();
-};
+  req.write(data)
+  req.end()
+}
